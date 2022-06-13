3fa9b621eb38d041e3e69c20b44e4369f1649ae7
skwhdeo <skwhdeo@gmail.com>
2022-06-14 00:50:09 +0900

    Update Result.java

---
+++ Result.java
@@ -0,0 +1,34 @@
+package com.lgcns.test;
+
+public class Result {
+	private String Result;
+	private String MessageID;
+	private String Message;
+
+	public Result() {
+	}
+
+	public String getResult() {
+		return Result;
+	}
+
+	public void setResult(String result) {
+		Result = result;
+	}
+
+	public String getMessageID() {
+		return MessageID;
+	}
+
+	public void setMessageID(String messageID) {
+		MessageID = messageID;
+	}
+
+	public String getMessage() {
+		return Message;
+	}
+
+	public void setMessage(String message) {
+		Message = message;
+	}
+}

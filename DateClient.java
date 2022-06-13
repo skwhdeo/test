2ef59d348560ad13e29b0208a8e92a4d5fd4f0e5
skwhdeo <skwhdeo@gmail.com>
2022-06-14 00:47:48 +0900

    Update DateClient.java

---
+++ DateClient.java
@@ -0,0 +1,25 @@
+package com.lgcns.test;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.http.HttpMethod;
+
+public class DateClient {
+
+	public static void main(String[] args) throws Exception {
+
+		HttpClient httpClient = new HttpClient();
+
+		httpClient.start();
+
+		ContentResponse contentRes =
+
+				httpClient.newRequest("http://127.0.0.1:8080/requestDate").method(HttpMethod.GET).send();
+
+		System.out.println(contentRes.getContentAsString());
+
+		httpClient.stop();
+
+	}
+
+}

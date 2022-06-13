8c3b6c9632812dd779b790ce68a35fe3871ea7f5
skwhdeo <skwhdeo@gmail.com>
2022-06-14 00:46:33 +0900

    Update BinaryFile2.java

---
+++ BinaryFile2.java
@@ -0,0 +1,42 @@
+package com.lgcns.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.util.Map;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.http.HttpMethod;
+
+public class BinaryFile2 {
+
+	public static void main(String[] args) throws Exception {
+
+		HttpClient httpClient = new HttpClient();
+
+		httpClient.start();
+
+		ContentResponse contentRes =
+
+				httpClient.newRequest("http://127.0.0.1:8080/requestFile2").method(HttpMethod.GET).send();
+
+		byte[] myByteArray = contentRes.getContent();
+
+		ByteArrayInputStream byteIn = new ByteArrayInputStream(myByteArray);
+
+		ObjectInputStream in = new ObjectInputStream(byteIn);
+
+		Map<String, byte[]> data2 = (Map<String, byte[]>) in.readObject();
+
+		try (FileOutputStream fos = new FileOutputStream("C:\\temp\\test.jpg")) {
+
+			fos.write(data2.get("file"));
+
+		}
+
+		httpClient.stop();
+
+	}
+
+}

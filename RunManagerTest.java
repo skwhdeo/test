d8fa8390cbf0e839c7ace3cf32fc9fb3312c18be
skwhdeo <skwhdeo@gmail.com>
2022-06-14 09:50:04 +0900

    Update RunManagerTest.java

--- RunManagerTest.java
+++ RunManagerTest.java
@@ -1,9 +1,8 @@
 package com.lgcns.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -14,35 +13,33 @@ class RunManagerTest {
 	@Test
 	void test() {
 		MyService service = new MyService();
-		File commands = new File("SAMPLE/SAMPLE.TXT");
-		File ans = new File("SAMPLE/CMP_CONSOLE.TXT");
-		try (BufferedReader br = new BufferedReader(new FileReader(commands))) {
-			try (BufferedReader br2 = new BufferedReader(new FileReader(ans))) {
-			    String line;
-			    String line_console;
-			    while ((line = br.readLine()) != null) {
-			    	
-			    		System.out.println("sam:"+line);		    		
-			    		if ((line_console = br2.readLine()) != null) {
-			    			System.out.println("ans:"+line_console);
-			    			assertEquals(line, line_console);
-		    				String line_ans = service.doCommand(line);
-		    				if(line_ans != null) {
-		    					if ((line_console = br2.readLine()) != null) {
-		    						System.out.println(line_console+"="+line_ans);
-		    						assertEquals(line_console, line_ans);
-		    					}
-		    				}
-			    		}			    		
-			    }
+		try (BufferedReader brIn = new BufferedReader(new FileReader("SAMPLE/SAMPLE.TXT"))) {
+			try (BufferedReader brOut = new BufferedReader(new FileReader("SAMPLE/CMP_CONSOLE.TXT"))) {
+				String line;
+				String line_console;
+				while ((line = brIn.readLine()) != null) {
+
+					System.out.println("sam:" + line);
+					if ((line_console = brOut.readLine()) != null) {
+						System.out.println("ans:" + line_console);
+						assertEquals(line, line_console);
+						String line_ans = service.doCommand(line);
+						if (line_ans != null) {
+							if ((line_console = brOut.readLine()) != null) {
+								System.out.println(line_console + "=" + line_ans);
+								assertEquals(line_console, line_ans);
+							}
+						}
+					}
+				}
 
 			} catch (IOException e) {
-			    e.printStackTrace();
-			}		    	
+				e.printStackTrace();
+			}
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		
+
 	}
 
 }
